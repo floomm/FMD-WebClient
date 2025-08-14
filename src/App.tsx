be0633fdb6d2659c2bb41fb3254fac12c935a6f8
Flum Silvan (floomm) <github.contact@mail.flum.tech>
2025-08-14 17:51:42 +0200

    Add dark/light mode toggle

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import {Route, Routes} from "react-router";
 import EmulatorPage from "./pages/EmulatorPage";
+import {ThemeProvider} from "@/components/theme-provider.tsx";
+import {ModeToggle} from "@/components/mode-toggle.tsx";
 
 function App() {
     return (
-        <>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
             <Routes>
                 <Route path="/emulator" element={<EmulatorPage/>}/>
             </Routes>
-        </>
+            <ModeToggle />
+        </ThemeProvider>
     )
 }
 
