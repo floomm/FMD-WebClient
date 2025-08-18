6d889e1a27a6e7210c6d4a85c9969070ae2fb8b7
Flum Silvan (floomm) <github.contact@mail.flum.tech>
2025-08-18 17:20:02 +0200

    Add simple pagination

--- src/components/ui/data-table.tsx
+++ src/components/ui/data-table.tsx
@@ -2,6 +2,7 @@ import {
     ColumnDef,
     flexRender,
     getCoreRowModel,
+    getPaginationRowModel,
     useReactTable,
 } from "@tanstack/react-table"
 
@@ -14,6 +15,14 @@ import {
     TableRow,
 } from "@/components/ui/table.tsx"
 import {useSidebar} from "@/components/ui/sidebar.tsx";
+import {useState} from "react";
+import {
+    Pagination,
+    PaginationContent,
+    PaginationItem,
+    PaginationNext,
+    PaginationPrevious
+} from "@/components/ui/pagination.tsx";
 
 interface DataTableProps<TData, TValue> {
     columns: ColumnDef<TData, TValue>[]
@@ -21,61 +30,95 @@ interface DataTableProps<TData, TValue> {
     data: TData[]
 }
 
-
 export function DataTable<TData, TValue>({columns, mobileColumns, data,}: Readonly<DataTableProps<TData, TValue>>) {
     const {isMobile} = useSidebar();
     const columnsToRender = isMobile ? (mobileColumns ?? columns) : columns;
+
+    const [pagination, setPagination] = useState({
+        pageIndex: 0, //initial page index
+        pageSize: 5, //default page size
+    });
 
     const table = useReactTable({
         data,
         columns: columnsToRender,
         getCoreRowModel: getCoreRowModel(),
-    })
+        getPaginationRowModel: getPaginationRowModel(),
+        onPaginationChange: setPagination,
+        state: {
+            pagination,
+        }
+    });
 
     return (
-        <div className="overflow-hidden rounded-md border">
-            <Table>
-                <TableHeader>
-                    {table.getHeaderGroups().map((headerGroup) => (
-                        <TableRow key={headerGroup.id}>
-                            {headerGroup.headers.map((header) => {
-                                return (
-                                    <TableHead key={header.id}>
-                                        {header.isPlaceholder
-                                            ? null
-                                            : flexRender(
-                                                header.column.columnDef.header,
-                                                header.getContext()
-                                            )}
-                                    </TableHead>
-                                )
-                            })}
-                        </TableRow>
-                    ))}
-                </TableHeader>
-                <TableBody>
-                    {table.getRowModel().rows.length ? (
-                        table.getRowModel().rows.map((row) => (
-                            <TableRow
-                                key={row.id}
-                                data-state={row.getIsSelected() && "selected"}
-                            >
-                                {row.getVisibleCells().map((cell) => (
-                                    <TableCell key={cell.id}>
-                                        {flexRender(cell.column.columnDef.cell, cell.getContext())}
-                                    </TableCell>
-                                ))}
+        <div>
+            <div className="overflow-hidden rounded-md border">
+                <Table>
+                    <TableHeader>
+                        {table.getHeaderGroups().map((headerGroup) => (
+                            <TableRow key={headerGroup.id}>
+                                {headerGroup.headers.map((header) => {
+                                    return (
+                                        <TableHead key={header.id}>
+                                            {header.isPlaceholder
+                                                ? null
+                                                : flexRender(
+                                                    header.column.columnDef.header,
+                                                    header.getContext()
+                                                )}
+                                        </TableHead>
+                                    )
+                                })}
                             </TableRow>
-                        ))
-                    ) : (
-                        <TableRow>
-                            <TableCell colSpan={columnsToRender.length} className="h-24 text-center">
-                                No results.
-                            </TableCell>
-                        </TableRow>
-                    )}
-                </TableBody>
-            </Table>
+                        ))}
+                    </TableHeader>
+                    <TableBody>
+                        {table.getRowModel().rows.length ? (
+                            table.getRowModel().rows.map((row) => (
+                                <TableRow
+                                    key={row.id}
+                                    data-state={row.getIsSelected() && "selected"}
+                                >
+                                    {row.getVisibleCells().map((cell) => (
+                                        <TableCell key={cell.id}>
+                                            {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                        </TableCell>
+                                    ))}
+                                </TableRow>
+                            ))
+                        ) : (
+                            <TableRow>
+                                <TableCell colSpan={columnsToRender.length} className="h-24 text-center">
+                                    No results.
+                                </TableCell>
+                            </TableRow>
+                        )}
+                    </TableBody>
+                </Table>
+            </div>
+            {table.getPageCount() > 1 && (
+                <Pagination className="my-4">
+                    <PaginationContent>
+                        {table.getCanPreviousPage() && (
+                            <PaginationItem>
+                                <PaginationPrevious onClick={() => {
+                                    table.previousPage();
+                                }}/>
+                            </PaginationItem>
+                        )}
+                        <PaginationItem>
+                            {table.getState().pagination.pageIndex + 1} / {table.getPageCount()}
+                        </PaginationItem>
+                        {table.getCanNextPage() && (
+                            <PaginationItem>
+                                <PaginationNext onClick={() => {
+                                    table.nextPage();
+                                }}/>
+                            </PaginationItem>
+                        )}
+                    </PaginationContent>
+                </Pagination>
+            )}
         </div>
     )
 }
