db6d8845ac6e492457d47b2cf0935e36399f5926
Flum Silvan (floomm) <github.contact@mail.flum.tech>
2025-10-02 17:54:44 +0200

    Implement new ScannerPage

--- src/pages/scanner-page.tsx
+++ src/pages/scanner-page.tsx
@@ -1,9 +1,28 @@
 import {BasePage} from "@/pages/base-page.tsx";
+import {RqJobsTable} from "@/components/ui/rq-jobs-table.tsx";
+import {Alert, AlertTitle} from "@/components/ui/alert.tsx";
+import {AlertCircleIcon} from "lucide-react";
+import {Button} from "@/components/ui/button.tsx";
+import {useNavigate} from "react-router";
 
 export function ScannerPage() {
+    const navigate = useNavigate();
+
     return (
-        <BasePage title="App Scanner">
-            <span>TODO</span>
+        <BasePage title="Recent App Scan Jobs">
+            <Alert className="flex items-center justify-center max-w-5xl">
+                <AlertCircleIcon/>
+                <AlertTitle className="flex items-center justify-center flex-wrap">
+                    If you wish to start new scans, navigate to the
+                    <Button onClick={() => void navigate("/firmwares")} className="px-1 py-0" variant="link">Firmwares</Button>
+                    or
+                    <Button onClick={() => void navigate("/apps")} className="px-1 py-0" variant="link">Apps</Button>
+                    page.
+                </AlertTitle>
+            </Alert>
+            <div className="flex items-center justify-center w-full max-w-7xl">
+                <RqJobsTable funcNames={["start_scan"]}/>
+            </div>
         </BasePage>
     );
 }
