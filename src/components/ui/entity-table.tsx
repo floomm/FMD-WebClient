2d825cb5eb6c6b46678e387a50085e85b801891f
Flum Silvan (floomm) <github.contact@mail.flum.tech>
2025-10-09 21:05:05 +0200

    Display objects in EntityTable

--- src/components/ui/entity-table.tsx
+++ src/components/ui/entity-table.tsx
@@ -13,24 +13,34 @@ export function EntityTable(
         <Table>
             <TableBody>
                 {Object.entries(entity)
-                    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                    .filter(([_, value]) => !value || typeof value !== "object") // show object if it's null, otherwise we don't display objects
                     .map(([key, value]) => (
                         <TableRow key={key}>
                             <TableCell className="font-medium">{key}</TableCell>
                             <TableCell className="text-muted-foreground whitespace-pre-wrap">
                                 {(() => {
-                                    let displayValue = value;
+                                    if (!value) {
+                                        return String(value);
+                                    }
+
+                                    if (typeof value === "object") {
+                                        try {
+                                            return JSON.stringify(value, null, 2);
+                                        } catch {
+                                            return "[Object]";
+                                        }
+                                    }
+
+                                    if (typeof value !== "string") {
+                                        return String(value);
+                                    }
 
                                     try {
                                         // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
                                         const parsed = JSON.parse(value);
-                                        displayValue = JSON.stringify(parsed, null, 2);
+                                        return JSON.stringify(parsed, null, 2);
                                     } catch {
-                                        // ignore, we leave it as string if parsing fails
+                                        return value;
                                     }
-
-                                    return displayValue;
                                 })()}
                             </TableCell>
                         </TableRow>
